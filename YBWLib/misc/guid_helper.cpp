fd4f695c9ca8ca904f6bd3ebae3961d238dcaf53
yangbowen <ybw1234567@hotmail.com>
2018-09-27 19:11:48 +0000

    Export GUID helper functions.

--- YBWLib/misc/guid_helper.cpp
+++ YBWLib/misc/guid_helper.cpp
@@ -2,18 +2,19 @@
 #include "stdafx.h"
 #include <guiddef.h>
 #include <string>
+#include "../YBWLib.h"
 #include "guid_helper.h"
 
-size_t func_hash_guid(const GUID* t) {
+YBWLIB_API size_t func_hash_guid(const GUID* t) {
 	std::hash<std::string> hash;
 	return hash(std::string((const char*)t, sizeof(GUID) / sizeof(char)));
 }
 
-bool func_equal_to_guid(const GUID* l, const GUID* r) {
+YBWLIB_API bool func_equal_to_guid(const GUID* l, const GUID* r) {
 	return IsEqualGUID(*l, *r);
 }
 
-bool func_less_than_guid(const GUID* l, const GUID* r) {
+YBWLIB_API bool func_less_than_guid(const GUID* l, const GUID* r) {
 	static_assert(sizeof(GUID) == 0x10, L"The size of GUID is not 0x10.");
 	static_assert(sizeof(uint32_t) == 0x4, L"The size of uint32_t is not 0x4.");
 	for (int i = 0; i < 0x4; ++i) {
--- YBWLib/misc/guid_helper.h
+++ YBWLib/misc/guid_helper.h
@@ -3,22 +3,23 @@
 #define _INCLUDED_YBWLIB_GUID_HELPER_H_
 #include <minwindef.h>
 #include <guiddef.h>
+#include "../YBWLib.h"
 
-size_t func_hash_guid(const GUID* t);
+YBWLIB_API size_t func_hash_guid(const GUID* t);
 struct hash_guid {
 	FORCEINLINE size_t operator()(const GUID& t) const {
 		return func_hash_guid(&t);
 	}
 };
 
-bool func_equal_to_guid(const GUID* l, const GUID* r);
+YBWLIB_API bool func_equal_to_guid(const GUID* l, const GUID* r);
 struct equal_to_guid {
 	FORCEINLINE bool operator()(const GUID& l, const GUID& r) const {
 		return func_equal_to_guid(&l, &r);
 	}
 };
 
-bool func_less_than_guid(const GUID* l, const GUID* r);
+YBWLIB_API bool func_less_than_guid(const GUID* l, const GUID* r);
 struct less_than_guid {
 	FORCEINLINE bool operator()(const GUID& l, const GUID& r) const {
 		return func_less_than_guid(&l, &r);
